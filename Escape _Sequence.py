297e020c8457c9bf78f000deb13dd1bc5c3c8958
Asif tyagi <59314914+Asiftyagi-kiet@users.noreply.github.com>
2020-10-23 21:09:30 +0530

    Create Escape _Sequence.py

---
+++ Escape _Sequence.py
@@ -0,0 +1,14 @@
+Escape Sequence       Meaning
+   \'                Single quote
+   \"               Double quote
+   \\"                backslash
+   \n                 new line
+   \t                   tab
+   \b                 backspace
+   
+   
+ praticale
+ 
+ print("line A\nline B \n line C")
+ print("name\tAsif Tyagi")
+ print("this is double backslash\\")
